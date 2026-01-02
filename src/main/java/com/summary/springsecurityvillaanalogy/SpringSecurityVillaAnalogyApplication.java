a88fdaa7c69166564afa82884033394b01c612f4
Kareem Emad <kareememad8561@gmail.com>
2026-01-02 13:36:01 +0200

    Initial commit

---
+++ src/main/java/com/summary/springsecurityvillaanalogy/SpringSecurityVillaAnalogyApplication.java
@@ -0,0 +1,13 @@
+package com.summary.springsecurityvillaanalogy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityVillaAnalogyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityVillaAnalogyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/summary/springsecurityvillaanalogy/SpringSecurityVillaAnalogyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.summary.springsecurityvillaanalogy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityVillaAnalogyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
